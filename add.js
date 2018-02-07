8005d39a2f3229762c8f624acb58ac5b57b20c76
Alan Alvarez <aalvarez@aliensoft.net>
2018-02-07 15:57:28 -0400

    Restructures solution into separate source files + Implements multiple-term sum

---
+++ add.js
@@ -0,0 +1,105 @@
+/**
+ * @type {number}
+ */
+const CARRY_OVER_DIGIT = '1';
+
+
+let memoized = {}; // should we use this?
+/**
+ * @typedef {Object} AddResult
+ * @property {string} digit 
+ * @property {boolean} hasCarryOver carry-over can be 1 at most. So using boolean for convenience
+ */
+
+/**
+ * Adds two single-digit (char) terms
+ * @param {string} a First Term
+ * @param {string} b Second Term
+ * @returns {AddResult}
+ */
+function singleCharAdd(a, b) {
+  const sum = parseInt(a) + parseInt(b); // this can be optimized using a sum table, or memoizing results
+  if (sum > 9) {
+    return {
+      digit: sum % 10,
+      hasCarryOver: true
+    }
+  } else {
+    return {
+      digit: sum,
+      hasCarryOver: false
+    }
+  }
+}
+
+/**
+ * Adds two multiple-digit (string) terms
+ * @param {string} a 
+ * @param {string} b 
+ * @returns {string}
+ */
+function twoTermsAdd(a, b) {
+  /**
+   * Approach: Runs through both array in reverse adding terms from both rows (if they exist)
+   * aIndex = Current index of a term
+   * bIndex = Current index of b term
+   * hasCarryOver = Whether the last singleCharAdd operation had a carryOver
+   */
+  let total = [];
+  for (
+    let aIndex = a.length-1, bIndex = b.length-1, hasCarryOver = false; 
+    aIndex > -1 || bIndex > -1 || hasCarryOver === true;
+    --aIndex, --bIndex
+  ) {
+    if (aIndex < 0 && bIndex < 0 && hasCarryOver == true) {
+      total.push(CARRY_OVER_DIGIT);
+      hasCarryOver = false;
+    } else if (aIndex < 0) {
+      if (hasCarryOver === true) {
+        const result = singleCharAdd(b[bIndex], CARRY_OVER_DIGIT);
+        total.push(result.digit);
+        hasCarryOver = result.hasCarryOver;
+      } else {
+        total.push(b[bIndex]);
+      }
+    } else if (bIndex < 0)  {
+      if (hasCarryOver === true) {
+        const result = singleCharAdd(a[aIndex], CARRY_OVER_DIGIT);
+        total.push(result.digit);
+        hasCarryOver = result.hasCarryOver;
+      } else {
+        total.push(a[aIndex]);
+      }
+    } else {
+      let result = singleCharAdd(a[aIndex], b[bIndex]);
+      if (hasCarryOver) {
+        result.digit = singleCharAdd(result.digit, CARRY_OVER_DIGIT).digit;
+      }
+      hasCarryOver = result.hasCarryOver;
+      total.push(result.digit);
+    }
+  }
+
+  return total.reverse().join('');
+}
+
+/**
+ * Adds an arbitrary amount of terms
+ * @param {...number} terms 
+ * @returns {string}
+ */
+function add(terms) {
+  // TODO: validate terms are all digits
+  const args = Array.prototype.slice.call(arguments); // convert arguments to real array
+  if (args.length == 0) {
+    return '0';
+  } else {
+    return args.reduce((total, term) => twoTermsAdd(total, term), '0');
+  }
+}
+
+module.exports = {
+  add,
+  twoTermsAdd,
+  singleCharAdd
+}
---
+++ tests.js
@@ -0,0 +1,68 @@
+const {add, singleCharAdd, twoTermsAdd} = require('./add');
+const assert = require('assert');
+
+function itShoudPerformSingleDigitAddition() {
+  try {
+    assert.deepStrictEqual(singleCharAdd('0', '0'), {digit: 0, hasCarryOver: false}, 'adding 0 + 0');
+    assert.deepStrictEqual(singleCharAdd('0', '000'), {digit: 0, hasCarryOver: false}, 'adding multiple 0s');
+    assert.deepStrictEqual(singleCharAdd('1', '3'), {digit: 4, hasCarryOver: false}, '1 + 3');
+    assert.deepStrictEqual(singleCharAdd('2', '8'), {digit: 0, hasCarryOver: true}, '2 + 8');
+    assert.deepStrictEqual(singleCharAdd('9', '9'), {digit: 8, hasCarryOver: true}, '9 + 9, max internal addition');
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+
+  return true;
+}
+
+function itShouldPerformTwoTermsAddition() {
+  try {
+    assert.deepStrictEqual(twoTermsAdd('12', '9'), '21');
+    assert.deepStrictEqual(twoTermsAdd('9', '12'), '21');
+    assert.deepStrictEqual(twoTermsAdd('19999', '22'), '20021');
+    assert.deepStrictEqual(twoTermsAdd('22', '19999'), '20021');
+    assert.deepStrictEqual(twoTermsAdd('44', '94'), '138');
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+
+  return true;
+}
+
+function itShouldPerformMultipleTermsAddition() {
+  try {
+    assert.deepStrictEqual(add('12', '9'), '21');
+    assert.deepStrictEqual(add('12', '9', '21'), '42');
+    assert.deepStrictEqual(add('264', '9', '2321', '42'), '2636');
+    assert.deepStrictEqual(add('00', '9', '2321', '42'), '2372');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const tests = [
+  itShoudPerformSingleDigitAddition,
+  itShouldPerformTwoTermsAddition,
+  itShouldPerformMultipleTermsAddition
+]
+
+/**
+ * Runs all tests
+ */
+function runTests() {
+  for (let i = 0; i < tests.length; ++i) {
+    console.log('Running test: ', tests[i].name);
+    if (tests[i]() === false) {
+      console.log('ERROR');
+      break;
+    } else {
+      console.log('ok');
+    }
+  }
+}
+
+module.exports = {
+  runTests
+}
